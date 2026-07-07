608efb1a656d8043850eb34893bdf1c47f3eb79a
Atqa Munzir <atqamz@gmail.com>
2026-07-07 22:58:54 +0700

    feat: package mirufm as a nix flake output
    Add packages.default building the mirufm binary with buildRustPackage,
    wrapping it with the same runtime libraries as the devShell and
    installing a .desktop entry so it can register as a folder handler.
    Document install and set-as-default steps in the README.

--- flake.nix
+++ flake.nix
@@ -25,7 +25,39 @@
           fontconfig
           freetype
         ];
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rust;
+          rustc = rust;
+        };
       in {
+        packages.default = rustPlatform.buildRustPackage {
+          pname = "mirufm";
+          version = "0.0.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "gpui-0.2.2" = "sha256-A4z6PwnAFOuzP6XymyLzOMwkDlxE3DpkSduyydlOUt8=";
+              "zed-font-kit-0.14.1-zed" = "sha256-KXygi0olNQi5yM8eaJVykNDtbPMDjT+cWPBF8UrtXR4=";
+              "zed-scap-0.0.8-zed" = "sha256-BihiQHlal/eRsktyf0GI3aSWsUCW7WcICMsC2Xvb7kw=";
+              "xim-ctext-0.3.0" = "sha256-pRT4Sz1JU9ros47/7pmIW9kosWOGMOItcnNd+VrvnpE=";
+            };
+          };
+
+          nativeBuildInputs = [ pkgs.pkg-config pkgs.makeWrapper ];
+          buildInputs = runtimeLibs;
+
+          cargoBuildFlags = [ "-p" "mirufm" ];
+          doCheck = false;
+
+          postInstall = ''
+            wrapProgram $out/bin/mirufm \
+              --set LD_LIBRARY_PATH ${pkgs.lib.makeLibraryPath runtimeLibs}
+            install -Dm444 ${./assets/mirufm.desktop} $out/share/applications/mirufm.desktop
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [ rust ] ++ runtimeLibs;
           nativeBuildInputs = [ pkgs.pkg-config ];
